afab401ce77eba93d11072dc9b9b2297b93a99dd
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2022-12-09 12:23:42 +0530

    Escape the & symbol and increase to 25

--- src/service/botAlerterService.js
+++ src/service/botAlerterService.js
@@ -3,9 +3,11 @@ import telegramApi from '../api/telegramApi.js';
 
 const alertTopPerformersAndLosers = async (isTopPerformers = true) => {
   const topPerformers = await bseService.getTopPerformersAndLosers(isTopPerformers);
-  const rows = topPerformers
-    .slice(0, 15)
-    .map((entry) => `${entry.scripname} : ${entry.change_percent}% : ₹${entry.ltradert}\n`).join('%0A');
+  let rows = topPerformers
+    .slice(0, 25)
+    .map((entry) => `${entry.scripname} : ${entry.change_percent}% : ₹${entry.ltradert}\n`)
+    .join('%0A');
+  rows = rows.replace("&"," ")
   const header = isTopPerformers ? 'BSE Gainers' : 'BSE Looser';
   const content = `${header}%0A-------------%0A${rows}%0A-------------%0A<a href="https://11akvs64nj.execute-api.ap-south-1.amazonaws.com/default/BseGainersBot"><b>Get Current</b></a>`;
   const response = await telegramApi.sendMessage(content);
