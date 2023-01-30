08e259e21f5ac25c6448eadc1c7d329208708549
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2023-01-30 16:41:02 +0530

    Adding gainers and loosers api

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-import botAlerterService from "./src/service/botAlerterService.js";
+import botAlerterService from './src/service/botAlerterService.js';
 
-export const handler = async () => {
-    const botAlerterResponse1 = await botAlerterService.alertTopPerformersAndLosers();
-    const botAlerterResponse2 = await botAlerterService.alertTopPerformersAndLosers(false);
-    return {
+// eslint-disable-next-line import/prefer-default-export
+export const handler = async (event) => {
+  const { operation } = event;
+
+  if (operation === 'echo') {
+    return (event.payload);
+  }
+  switch (operation) {
+    case 'gainers':
+      return {
         statusCode: 200,
-        body: JSON.stringify(`Sent at ${new Date()} with Response ${botAlerterResponse1} ${botAlerterResponse2}`)
-    }
-}
+        body: await botAlerterService.getTopPerfomers(true),
+      };
+    case 'loosers':
+      return {
+        statusCode: 200,
+        body: await botAlerterService.getTopPerfomers(false),
+      };
+    default:
+      const botAlerterResponse1 = await botAlerterService.alertTopPerformersAndLosers();
+      const botAlerterResponse2 = await botAlerterService.alertTopPerformersAndLosers(false);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(`Sent at ${new Date()} with Response ${botAlerterResponse1} ${botAlerterResponse2}`),
+      };
+  }
+};
--- src/service/botAlerterService.js
+++ src/service/botAlerterService.js
@@ -4,16 +4,20 @@ import telegramApi from '../api/telegramApi.js';
 const alertTopPerformersAndLosers = async (isTopPerformers = true) => {
   const topPerformers = await bseService.getTopPerformersAndLosers(isTopPerformers);
   let rows = topPerformers
-    .slice(0, 25)
+    .slice(0, 30)
     .map((entry) => `${entry.scripname} : ${entry.change_percent}% : ₹${entry.ltradert}\n`)
     .join('%0A');
-  rows = rows.replace("&"," ")
+  rows = rows.replace('&', ' ');
   const header = isTopPerformers ? 'BSE Gainers' : 'BSE Looser';
   const content = `${header}%0A-------------%0A${rows}%0A-------------%0A<a href="https://11akvs64nj.execute-api.ap-south-1.amazonaws.com/default/BseGainersBot"><b>Get Current</b></a>`;
   const response = await telegramApi.sendMessage(content);
   return response.statusText;
 };
 
+// eslint-disable-next-line max-len
+const getTopPerfomers = async (isTopPerformers = true) => bseService.getTopPerformersAndLosers(isTopPerformers);
+
 export default {
   alertTopPerformersAndLosers,
+  getTopPerfomers,
 };
