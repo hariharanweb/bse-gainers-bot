735dea228168be4e6254b231aba5b71de00a3293
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2023-08-07 16:09:06 +0530

    Revert to original API:

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import botAlerterService from './service/botAlerterService.js';
 
 const send = async () => {
+  await botAlerterService.alertTopPerformersAndLosers(true);
   await botAlerterService.alertTopPerformersAndLosers(false);
-  setTimeout(async () => {
-    await botAlerterService.alertTopPerformersAndLosers(true);
-  }, 10000);
 };
 send();
