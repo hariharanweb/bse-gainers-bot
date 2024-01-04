33139779957a62b0b4c0ca4c653c988985f56eec
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2024-01-04 15:18:17 +0530

    Experiment with expressjs

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-import botAlerterService from './src/service/botAlerterService.js';
+import serverless from 'serverless-http';
+import server from './src/server.js';
+
+const serverlessHandler = serverless(server);
 
 // eslint-disable-next-line import/prefer-default-export
-export const handler = async (event) => {
-  let operation;
-  if (event && event.queryStringParameters && event.queryStringParameters.operation) {
-    operation = event.queryStringParameters.operation;
-  }
-  if (operation === 'echo') {
-    return (event.payload);
-  }
-  switch (operation) {
-    case 'gainers':
-      return botAlerterService.getTopPerfomers(true);
-    case 'loosers':
-      return botAlerterService.getTopPerfomers(false);
-    default:
-      const botAlerterResponse1 = await botAlerterService.alertTopPerformersAndLosers();
-      const botAlerterResponse2 = await botAlerterService.alertTopPerformersAndLosers(false);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(`Sent at ${new Date()} with Response ${botAlerterResponse1} ${botAlerterResponse2}`),
-      };
-  }
+export const handler = (event, context, callback) => {
+  const response = serverlessHandler(event, context, callback);
+  return response;
 };
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bseService from './service/bseService.js';
+import botAlerterService from './service/botAlerterService.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is listening on port ${port}.`));
+
+app.use((_, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
+app.get('/', async (_, res) => {
+  const botAlerterResponse1 = await botAlerterService.alertTopPerformersAndLosers();
+  const botAlerterResponse2 = await botAlerterService.alertTopPerformersAndLosers(false);
+  res.send(
+    `Sent at ${new Date()} with Response ${botAlerterResponse1} ${botAlerterResponse2}`,
+  );
+});
+app.get('/market', async (_, res) => {
+  const topPerformers = await bseService.getTopPerformersAndLosers(true);
+  res.send(topPerformers);
+});
+
+export default app;
