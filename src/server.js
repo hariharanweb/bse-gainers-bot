5ce23b2c3a11b06e571a0e7042c6c8d6c3ae590b
Hariharan <hariharanweb@gmail.com>
2024-01-09 12:46:37 +0530

    Fixing the controller to give right JSONs:

--- src/server.js
+++ src/server.js
@@ -3,12 +3,8 @@ import bseService from './service/bseService.js';
 import botAlerterService from './service/botAlerterService.js';
 
 const app = express();
-
-console.log('ENV', process.env.ENVIRONMENT)
-if(process.env.ENVIRONMENT != "lambda"){
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Server is listening on port ${port}.`));
-} 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is listening on port ${port}.`));
 
 app.use((_, res, next) => {
   res.setHeader('Content-Type', 'application/json');
@@ -17,7 +13,7 @@ app.use((_, res, next) => {
 
 app.get('/', async (req, res) => {
   res.send(
-    `Sent at ${new Date()} ${JSON.stringify(req)}`,
+    {data : `Sent at ${new Date()} to path ${req.path}`}
   );
 })
 
@@ -25,7 +21,7 @@ app.get('/alert', async (_, res) => {
   const botAlerterResponse1 = await botAlerterService.alertTopPerformersAndLosers();
   const botAlerterResponse2 = await botAlerterService.alertTopPerformersAndLosers(false);
   res.send(
-    `Sent at ${new Date()} with Response ${botAlerterResponse1} ${botAlerterResponse2}`,
+    {data :`Sent at ${new Date()} with Response ${botAlerterResponse1} ${botAlerterResponse2}`},
   );
 });
 app.get('/market', async (_, res) => {
