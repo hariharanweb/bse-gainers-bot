710b1a5e9782816b7c0dc1dc314e4458c842092c
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2024-05-02 11:36:07 +0530

    Get gainers and loosers parallely to avoid lambda timeouts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import botAlerterService from './service/botAlerterService.js';
 
 const send = async () => {
-  await botAlerterService.alertTopPerformersAndLosers(true);
-  await botAlerterService.alertTopPerformersAndLosers(false);
+  const gainerTask = botAlerterService.alertTopPerformersAndLosers(true);
+  const looserTask = botAlerterService.alertTopPerformersAndLosers(false);
+  await Promise.all([
+    gainerTask, looserTask,
+  ]);
 };
+
 send();
