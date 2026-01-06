31a5cfd27029e71c26c6531ce885a93e9de6ab7d
Hariharan Thiagarajan <hariharanweb@gmail.com>
2026-01-06 17:09:00 +0530

    Log the api call again

--- src/api/bseApi.js
+++ src/api/bseApi.js
@@ -1,19 +1,18 @@
 import fetch from 'node-fetch';
 import https from 'https';
 
-const agent = new https.Agent({
-  minVersion: 'TLSv1.2',
-  maxVersion: 'TLSv1.3',
-});
+// const agent = new https.Agent({
+//   minVersion: 'TLSv1.2',
+//   maxVersion: 'TLSv1.3',
+// });
 
 const getTopPerformersAndLoosers = async (isTopPerformer) => {
   const type = isTopPerformer ? 'gainer' : 'loser';
   try {
     console.log('**** Calling the API *****');
     const response = await fetch(`https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserDataeqto/w?GLtype=${type}&IndxGrp=group&IndxGrpval=A&orderby=all`, {
-      agent,
       headers: {
-        'REFERER':'https://www.bseindia.com/',
+        'referer':'https://www.bseindia.com/',
       },
     });
     console.log('**** End the API *****', response);
